681f08c6065849c1e204545f6e9bcf80a1f0b451
Pranpran3 <pranavleaps@gmail.com>
2025-06-16 13:02:03 -0700

    Summer Camp stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SummerCamp/random.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SummerCamp/random.java
@@ -1,24 +1,19 @@
 package org.firstinspires.ftc.teamcode.SummerCamp;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp (name = "random")
-public class random extends OpMode {
+public class random extends LinearOpMode {
 
     DcMotor motor;
 
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
         motor = hardwareMap.get(DcMotor.class,"motor");
-    }
-
-    @Override
-    public void loop() {
-
-        if (gamepad1.a) {
-            motor.setPower(0.05);
-        }
+        waitForStart();
+        motor.setPower(1);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SummerProject/PranOp.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.SummerProject;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "PranOp")
+public class PranOp extends LinearOpMode {
+
+    DcMotor fl;
+    DcMotor fr;
+    DcMotor bl;
+    DcMotor br;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        bl = hardwareMap.get(DcMotor.class, "bl");
+        br = hardwareMap.get(DcMotor.class, "br");
+
+    }
+}
